05ecae45045fce76dc025012e1f711dfcd46c82c
AdsonGuyllan <48564293+AdsonGuyllan@users.noreply.github.com>
2021-12-14 17:27:03 -0300

    Condições (Simples e composta)

---
+++ aula4/aula4.js
@@ -0,0 +1,7 @@
+// Condição simples
+var v = 60.2
+console.log(`Você está diriginddo a ${v}km/h`)
+if(v > 60){
+    console.log('Limite de velocidade ultrapassado. MULTADO!')
+}
+console.log('Digija com cuidado!')
